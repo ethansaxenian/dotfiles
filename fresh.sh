130f62d57c7d5fd8a422c75da5750d399568e112
Ethan Saxenian <esaxenian@middlebury.edu>
2022-01-21 18:08:32 -0500

    dotfiles path

--- fresh.sh
+++ fresh.sh
@@ -1,27 +1,30 @@
 #!/bin/sh
 
+# Path to your dotfiles.
+export DOTFILES=$HOME/.dotfiles
+
 if [[ "$OSTYPE" =~ ^darwin ]]; then
-	$HOME/.dotfiles/configure-mac.sh
+	$DOTFILES/configure-mac.sh
 fi
 
 if [[ "$OSTYPE" =~ ^linux ]]; then
-	$HOME/.dotfiles/configure-arch.sh
+	$DOTFILES/configure-arch.sh
 fi
 
 echo "Symlinking .zshrc..."
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
 rm -rf $HOME/.zshrc
-ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+ln -s $DOTFILES/.zshrc $HOME/.zshrc
 
 echo "Symlinking .gitconfig..."
 # Removes .gitconfig from $HOME (if it exists) and symlinks the .gitconfig file from the .dotfiles
 rm -rf $HOME/.gitconfig
-ln -s $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
+ln -s $DOTFILES/.gitconfig $HOME/.gitconfig
 
 echo "Symlinking .vimrc..."
 # Removes .vimrc from $HOME (if it exists) and symlinks the .vimrc file from the .dotfiles
 rm -rf $HOME/.vimrc
-ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
+ln -s $DOTFILES/.vimrc $HOME/.vimrc
 
 echo "Installing vim-plug..."
 # install vim-plug
@@ -31,4 +34,4 @@ echo "Symlinking vim colorschemes..."
 # add custom colorscheme
 rm -rf $HOME/.vim/colors/
 mkdir $HOME/.vim/colors/ -p
-ln -s $HOME/.dotfiles/vimcolors/* $HOME/.vim/colors
+ln -s $DOTFILES/vimcolors/* $HOME/.vim/colors
