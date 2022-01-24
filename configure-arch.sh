446a4d4e56394fcd24a5e534eb718aa2d65ee8f4
Ethan Saxenian <esaxenian@middlebury.edu>
2022-01-24 12:52:24 -0500

    add additional packages to linux setup

--- configure-arch.sh
+++ configure-arch.sh
@@ -3,7 +3,7 @@
 echo "Setting up Arch Linux..."
 
 echo "Installing packages..."
-sudo pacman --needed -S code firefox gcc gdb ghex git make man-db man-pages nasm openssh strace tree valgrind vim zsh
+sudo pacman --needed -S code firefox gcc gdb ghex git make man-db man-pages nasm openssh strace tree valgrind vim zsh wireshark-qt net-tools iputils
 
 echo "Symlinking .gdbinit..."
 rm -rf $HOME/.gdbinit
