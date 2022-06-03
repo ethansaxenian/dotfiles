5b11f47f2c5c12c5a018ec31abee556dbefcb14c
Ethan Saxenian <esaxenian@middlebury.edu>
2022-06-03 16:26:59 -0400

    fix vim order

--- fresh.sh
+++ fresh.sh
@@ -13,10 +13,6 @@ rm -rf $HOME/.gitconfig
 ln -s $DOTFILES/.gitconfig $HOME/.gitconfig
 
 echo "Setting up vim configuration..."
-# Removes .vimrc from $HOME (if it exists) and symlinks the .vimrc file from the .dotfiles
-rm -rf $HOME/.vimrc
-ln -s $DOTFILES/.vimrc $HOME/.vimrc
-
 # install vim-plug and plugins
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall
@@ -27,6 +23,10 @@ mkdir $HOME/.vim/undo $HOME/.vim/swp
 rm -rf $HOME/.vim/colors
 mkdir $HOME/.vim/colors
 ln -s $DOTFILES/mycolors.vim $HOME/.vim/colors/mycolors.vim
+
+# Removes .vimrc from $HOME (if it exists) and symlinks the .vimrc file from the .dotfiles
+rm -rf $HOME/.vimrc
+ln -s $DOTFILES/.vimrc $HOME/.vimrc
 
 # use zsh
 chsh -s $(which zsh)
