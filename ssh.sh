4f8b075086fe38c40a7c294362e2b49140797e16
Ethan Saxenian <esaxenian@middlebury.edu>
2022-06-06 10:28:51 -0400

    zsh

--- ssh.sh
+++ ssh.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env zsh
+
 echo "Generating a new SSH key for GitHub..."
 # https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
 
