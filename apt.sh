58bf08ff9d3fe01462b9aee07334bad9f1d66853
Ethan Saxenian <esaxenian@middlebury.edu>
2022-08-31 12:27:47 -0400

    remove symlink to bat

--- apt.sh
+++ apt.sh
@@ -4,10 +4,6 @@ echo "Installing packages with apt..."
 sudo apt update
 sudo apt install -y bat firefox gcc gdb git htop make spice-vdagent spice-webdavd terminator tree vim zsh
 
-# 'bat' is installed as 'batcat' so create a symlink to 'bat'
-mkdir -p ~/.local/bin
-ln -s /usr/bin/batcat ~/.local/bin/bat
-
 echo "Installing vscode..."
 curl -L https://aka.ms/linux-arm64-deb --output code_arm64.deb
 sudo apt install -y ./code_arm64.deb
