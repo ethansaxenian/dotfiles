7ec40b09f7ce26819aa004436696741eb968107b
Ethan Saxenian <ethansaxenian@gmail.com>
2022-10-16 21:30:18 -0400

    add fd to apt config

--- scripts/apt.sh
+++ scripts/apt.sh
@@ -2,10 +2,14 @@
 
 echo "Installing packages with apt..."
 sudo apt update
-sudo apt install -y bat fd firefox fzf gcc gdb git htop make neofetch net-tools nmap openssh-server spice-vdagent spice-webdavd terminator tree vim zoxide zsh
+sudo apt install -y bat fd-find firefox fzf gcc gdb git htop make neofetch net-tools nmap openssh-server spice-vdagent spice-webdavd terminator tree vim zoxide zsh
 
 if test ! $(command -v bat); then
   sudo ln -s /usr/bin/batcat /usr/bin/bat
+fi
+
+if test ! $(command -v fdfind); then
+  sudo ln -s /usr/bin/fdfind /usr/bin/fd
 fi
 
 if test ! $(command -v code); then
