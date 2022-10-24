ceeb19ac565dd0bc86998a3025b30e0565f72a15
Ethan Saxenian <ethansaxenian@gmail.com>
2022-10-24 17:48:38 -0400

    remove fzf from pi config

--- scripts/raspberrypi.sh
+++ scripts/raspberrypi.sh
@@ -2,7 +2,7 @@
 
 # apt
 sudo apt update
-sudo apt install -y bat fd-find fzf gcc gdb git htop make neofetch net-tools nmap openssh-server python3 python3-pip tree vim zoxide zsh
+sudo apt install -y bat fd-find gcc gdb git htop make neofetch net-tools nmap openssh-server python3 python3-pip tree vim zoxide zsh
 
 sudo ln -s /usr/bin/batcat /usr/bin/bat
 sudo ln -s /usr/bin/fdfind /usr/bin/fd
