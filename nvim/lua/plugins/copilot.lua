2f9cdfdb382539ff9c417faba98e53f2a4049697
Ethan Saxenian <ethansaxenian+github@proton.me>
2024-04-13 00:56:18 -0400

    copilot

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,8 @@
+-- don't autosuggest with copilot - only when <M-\> is pressed
+vim.g.copilot_filetypes = {
+  ["*"] = false,
+}
+
+return {
+  { "github/copilot.vim" },
+}
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,5 +1,4 @@
 return {
-  "github/copilot.vim",
   "tpope/vim-commentary",
   "romainl/vim-cool",
   "tpope/vim-surround",
