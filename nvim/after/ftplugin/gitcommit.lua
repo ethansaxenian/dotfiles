1fbcf55e2afe3088425b1330417fa406c840e9b9
Ethan Saxenian <ethansaxenian+github@proton.me>
2025-03-13 22:47:45 -0400

    fix annoying gitcommit cursor

--- nvim/after/ftplugin/gitcommit.lua
+++ nvim/after/ftplugin/gitcommit.lua
@@ -1,2 +1,11 @@
 vim.wo.wrap = true
 vim.wo.spell = true
+
+-- move cursor to the first line
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    vim.fn.cursor(1, 1)
+  end,
+  group = vim.api.nvim_create_augroup("gitconfig", { clear = true }),
+  pattern = "*",
+})
