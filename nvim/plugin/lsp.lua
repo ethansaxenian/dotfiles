b5ddb24638aec7a9f0c9d3446d74df1a6617bd2a
Ethan Saxenian <ethansaxenian+github@proton.me>
2025-07-21 09:40:39 -0400

    default lsp kepmaps

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -4,11 +4,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
     local opts = { buffer = event.buf }
 
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
     vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
-    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
     vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
     vim.keymap.set({ "n", "i" }, "<C-k>", vim.lsp.buf.signature_help, opts)
   end,
